e870e5787532e488b7f93835a452046f2640bf3e
akshayt3112005-sudo <akshayt3112005@gmail.com>
2026-08-09 01:44:11 +0530

    Restore urls from Samiksha version

--- sirisapp/urls.py
+++ sirisapp/urls.py
@@ -4,5 +4,10 @@ urlpatterns = [
     path('index/',views.index,name='index'),
     path('About/',views.About,name='About'),
     path('service/',views.service,name='service'),
-    
+    path('navbar/',views.navbar,name='navbar'),
+    path('traffic/',views.traffic,name='traffic'),
+    path('contaner1/',views.contaner1,name='contaner1'),
+    path('contaner2/',views.contaner2,name='contaner2'),
+    path('contact/',views.contact,name='contact'),
+    path('footer/', views.footer, name='footer'),
 ]    
