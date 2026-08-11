3d4973856132431d14cb164786c197dc7256a39a
akshayt3112005-sudo <akshayt3112005@gmail.com>
2026-08-11 16:21:33 +0530

    Update main

--- sirisapp/views.py
+++ sirisapp/views.py
@@ -30,10 +30,6 @@ def contaner2(request):
 def footer(request):
     return render(request, 'footer.html')
 
-
-
-
-
 def contact(request):
 
     if request.method == "POST":
@@ -67,3 +63,139 @@ Address / Project Location: {address}
         })
 
     return render(request, "contact.html")
+
+def Attendance(request):
+    return render(request,'Attendance.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def Cabin(request):
+    return render(request, 'Cabin.html')
+
+def Complete_Security(request):
+    return render(request, 'Complete Security.html')
+
+def cone(request):
+    return render(request, 'cone.html')
+
+def construction(request):
+    return render(request, 'construction.html')
+
+def Container_Houses(request):
+    return render(request, 'Container Houses.html')
+
+def Energy(request):
+    return render(request, 'Energy.html')
+
+def Executive_Office_Container(request):
+    return render(request, 'Executive Office Container.html')
+
+def flatpack(request):
+    return render(request, 'flatpack.html')
+
+def Food_Hospitality(request):
+    return render(request, 'Food & Hospitality .html')
+
+def Hotel_Rooms(request):
+    return render(request, 'Hotel Rooms.html')
+
+def Luxury_Modular_Site_Offices(request):
+    return render(request, 'Luxury Modular Site Offices.html')
+
+def luxuryvilla(request):
+    return render(request, 'luxuryvilla.html')
+
+def metal(request):
+    return render(request, 'metal.html')
+
+def Mobile_Hospitals(request):
+    return render(request, 'Mobile_Hospitals.html')
+
+def Mobile_Office(request):
+    return render(request, 'Mobile_Office.html')
+
+def mojo(request):
+    return render(request, 'mojo.html')
+
+def MS_Bunk_Bed_Container(request):
+    return render(request, 'MS Bunk Bed Container.html')
+
+def MS_Crowd(request):
+    return render(request, 'MS Crowd.html')
+
+def MS_STOP(request):
+    return render(request, 'MS STOP.html')
+
+def Municipal_Garbage(request):
+    return render(request, 'Municipal Garbage.html')
+
+def office_metal_container(request):
+    return render(request, 'office metal container.html')
+
+def office_and_home(request):
+    return render(request, 'office-and-home.html')
+
+def office(request):
+    return render(request, 'office.html')
+
+def oil(request):
+    return render(request, 'oil.html')
+
+def Plastic_Safety_Products(request):
+    return render(request, 'Plastic Safety Products.html')
+
+
+def PlasticSpeedBreaker(request):
+    return render(request, 'PlasticSpeedBreaker.html')
+
+def Porta_Cabins(request):
+    return render(request, 'Porta Cabins .html')
+
+def Portable_Gym(request):
+    return render(request, 'Portable Gym .html')
+
+def Portable_Security_Guard_Cabins(request):
+    return render(request, 'Portable Security Guard Cabins.html')
+
+def Portable_Toilet_Container(request):
+    return render(request, 'Portable Toilet Container.html')
+
+def Reefer(request):
+    return render(request, 'Reefer.html')
+
+def Restaurant_Container(request):
+    return render(request, 'Restaurant Container.html')
+
+def Rubber_Road_Signages(request):
+    return render(request, 'Rubber Road Signages.html')
+
+def RubberSpeedBreaker(request):
+    return render(request, 'RubberSpeedBreaker.html')
+
+def SandandWater(request):
+    return render(request, 'SandandWater.html')
+
+def Sanitation_Hygiene_Solutions(request):
+    return render(request, 'Sanitation & Hygiene Solutions.html')
+
+def single(request):
+    return render(request, 'single.html')
+
+def SSQueueManager(request):
+    return render(request, 'SSQueueManager.html')
+
+def Standard_Office(request):
+    return render(request, 'Standard Office .html')
+
+def Swimming_Pool_Containers(request):
+    return render(request, 'Swimming Pool Containers.html')
+
+def Toilet_on_Construction_Site(request):
+    return render(request, 'Toilet on Construction Site.html')
+
+def Trafic_Mirrors(request):
+    return render(request, 'Trafic Mirrors.html')
+
+def Wildlife(request):
+    return render(request, 'Wildlife.html')
